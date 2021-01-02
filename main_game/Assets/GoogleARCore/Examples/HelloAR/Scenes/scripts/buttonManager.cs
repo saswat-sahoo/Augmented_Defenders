b03e011a97d04fbfa7348069533ee0eb3fc8547d
saswat-sahoo <68820601+saswat-sahoo@users.noreply.github.com>
2021-01-03 01:54:21 +0530

    basic working model
    working model#1

---
+++ main_game/Assets/GoogleARCore/Examples/HelloAR/Scenes/scripts/buttonManager.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class buttonManager : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+
+    public void play()
+    {
+        SceneManager.LoadScene(2);
+    }
+
+    public void quit()
+    {
+        Application.Quit();
+    }
+
+    public void instructions()
+    {
+        SceneManager.LoadScene(1);
+    }
+
+    public void back()
+    {
+        SceneManager.LoadScene(0);
+    }
+}
